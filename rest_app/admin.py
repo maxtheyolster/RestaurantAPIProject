c01e94bbd58c0a82b0958ad09720af4e5bcaa7a8
Max <maxbaatar37@gmail.com>
2023-09-26 15:24:19 -0400

    Added Serializers for the Models

--- rest_app/admin.py
+++ rest_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Category, MenuItem
+from .models import Category, MenuItem, Cart, OrderItem, Order
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
--- rest_app/serializers.py
+++ rest_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MenuItem, Category
+from .models import MenuItem, Category, Cart, Order, OrderItem
 from django.contrib.auth.models import User, Group
 
 
@@ -16,3 +16,50 @@ class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
         model = MenuItem
         field = ['id', 'title', 'price', 'featured']
         depth = 1
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        default=serializers.CurrentUserDefault(),
+        read_only=True
+    )
+    unit_price = serializers.DecimalField(decimal_places=2, max_digits=6, default=0.0)
+    price = serializers.DecimalField(decimal_places=2, max_digits=6, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'order', 'menuitem', 'quantity', 'unit_price', 'price']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        default=serializers.CurrentUserDefault(),
+        read_only=True,
+    )
+    delivery_crew = serializers.PrimaryKeyRelatedField(queryset=User.objects.none())
+    order_items = OrderItemSerializer(many=True, read_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        delivery_crew_group = Group.objects.get(name='Delivery Crew')
+        delivery_crew_users = delivery_crew_group.user_set.all()
+        self.fields['delivery_crew'].queryset = delivery_crew_users
+
+    class Meta:
+        model = Order
+        fields = ['id', ' user', 'delivery_crew', 'status', 'total', 'date', 'order_items']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.ReadOnlyField()
+    password = serializers.ReadOnlyField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'password']
