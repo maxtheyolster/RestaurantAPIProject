b8515b250bd5583bc78a8fb497b4a3603ff1bd95
Max <maxbaatar37@gmail.com>
2023-09-28 11:34:29 -0400

    Added the URL paths for the Views

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleItemView.as_view()),
+    path('category/<int:pk>', views.category_detail.as_view(), name='category-detail'),
+    path('groups/manager/users', views.ManagersView.as_view()),
+    path('groups/manager/users/<int:pk>', views.ManagerRemoveView.as_view()),
+    path('groups/delivery-crew/users', views.DeliveryCrewView.as_view()),
+    path('groups/delivery-crew/users/<int:pk>', views.DeliveryCrewRemoveView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
+    path('orders', views.OrdersView.as_view()),
+    path('orders/<int:pk>', views.OrderIdView.as_view()),
+    path('orders/<int:pk>/order-items', views.OrderItemsView.as_view()),
 ]
